8eb98c5bf2aa83c9d071d9d6e5116bc779faee62
amac81 <arnaldocanelas@gmail.com>
2023-12-07 10:22:16 +0000

    Program update

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 /**
-* Este programa ....
+* Este programa le os dados de um departamento, de endereço e dos seus empregados. 
+* Gera um relatório de folha de pagamento
 * 
 * @author  Arnaldo Canelas
 * @version 1.0
@@ -12,6 +13,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -20,9 +23,19 @@ public class Program {
 		//Objeto Scanner para entrada de dados, a partir de System.in 
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("teste");
+		Department department = new Department();
+		
+		
 		
 		sc.close();
+		
+		showReport(department);
+	}
+	
+	private static void showReport(Department department) {
+		// MOSTRAR NA TELA OS DADOS DO RELATÓRIO
+		System.out.println("teste");
 	}
 		
 }
+;
