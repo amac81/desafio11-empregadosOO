6e8b15f818be5dd60c21ebeedf7493ee4c43e0de
amac81 <arnaldocanelas@gmail.com>
2023-12-07 10:45:12 +0000

    Atualização classes Employee e Address

--- src/entities/Address.java
+++ src/entities/Address.java
@@ -3,4 +3,28 @@ package entities;
 public class Address {
 	public String email;
 	public String phone;
+
+	public Address() {}
+
+	public Address(String email, String phone) {
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
 }
